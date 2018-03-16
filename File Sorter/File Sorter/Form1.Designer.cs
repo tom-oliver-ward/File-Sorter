019206c60141f7a36a181f809020ffdf7106a114
tom-oliver-ward <37127495+tom-oliver-ward@users.noreply.github.com>
2018-03-16 13:16:19 +1300

    Added existing Files

---
+++ File Sorter/File Sorter/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace File_Sorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Files2Convert = new System.Windows.Forms.ListBox();
+            this.ConvertB = new System.Windows.Forms.Button();
+            this.FilenumTB = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Files2Convert
+            // 
+            this.Files2Convert.AllowDrop = true;
+            this.Files2Convert.FormattingEnabled = true;
+            this.Files2Convert.Location = new System.Drawing.Point(27, 31);
+            this.Files2Convert.Name = "Files2Convert";
+            this.Files2Convert.Size = new System.Drawing.Size(700, 95);
+            this.Files2Convert.TabIndex = 0;
+            this.Files2Convert.SelectedIndexChanged += new System.EventHandler(this.Files2Convert_SelectedIndexChanged);
+            // 
+            // ConvertB
+            // 
+            this.ConvertB.Location = new System.Drawing.Point(298, 132);
+            this.ConvertB.Name = "ConvertB";
+            this.ConvertB.Size = new System.Drawing.Size(140, 23);
+            this.ConvertB.TabIndex = 2;
+            this.ConvertB.Text = "Convert";
+            this.ConvertB.UseVisualStyleBackColor = true;
+            this.ConvertB.Click += new System.EventHandler(this.ConvertB_Click);
+            // 
+            // FilenumTB
+            // 
+            this.FilenumTB.Location = new System.Drawing.Point(298, 162);
+            this.FilenumTB.Name = "FilenumTB";
+            this.FilenumTB.Size = new System.Drawing.Size(140, 20);
+            this.FilenumTB.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Drag Files Here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.FilenumTB);
+            this.Controls.Add(this.ConvertB);
+            this.Controls.Add(this.Files2Convert);
+            this.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Files2Convert;
+        private System.Windows.Forms.Button ConvertB;
+        private System.Windows.Forms.TextBox FilenumTB;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ File Sorter/File Sorter/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Sorter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Files2Convert.DragDrop += new
+           System.Windows.Forms.DragEventHandler(this.Files2Convert_DragDrop);
+            this.Files2Convert.DragEnter += new
+           System.Windows.Forms.DragEventHandler(this.Files2Convert_DragEnter);
+        }
+
+        private void Files2Convert_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Files2Convert_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Files2Convert_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+                Files2Convert.Items.Add(s[i]);
+        }
+
+        private void ConvertB_Click(object sender, EventArgs e)
+        {
+       
+         int length = System.Convert.ToInt32(System.Convert.ToDouble(Files2Convert.Items.Count.ToString()));
+         
+         //FilenumTB.Text = "Processing file 0 of" + length;            
+         Application.DoEvents();
+            for (int i=0;i<length;i++)
+            {
+                FilenumTB.Text = "Processing file " + (i+1) + " of " + length;
+                Application.DoEvents();
+                ConvertFile(i);
+
+            }
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show("Complete","", buttons);
+        
+        }
+
+        private void ConvertFile(int i)
+        {
+            String Filename = (String)Files2Convert.Items[i];
+            int FilenameL = Filename.Length;
+            String input = File.ReadAllText(Filename);
+            int pos=0;
+            int stop = 0;
+            int end;
+            string line;
+            string RamanS;
+            string DarkS;
+            int k = 1;
+
+            while (k != -1)
+            {
+                pos = k;
+                k = Filename.IndexOf("\\", k + 1);
+            }
+            string name = Filename.Substring(pos + 1, FilenameL - pos - 1);
+            string path = Filename.Substring(0, pos);
+            string newPath = path + "\\Processed\\" + name;
+
+            System.IO.Directory.CreateDirectory(path + "\\Processed\\");
+            pos = input.IndexOf("\n192;");
+
+            end = input.IndexOf("\n1985;");
+            
+            while (pos < end )
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    pos = input.IndexOf(";", pos) + 1;
+                }
+                stop = input.IndexOf(";", pos);
+                RamanS = input.Substring(pos, stop-pos);
+
+                for (int j = 0; j < 4; j++)
+                {
+                    pos = input.IndexOf(";", pos) + 1;
+
+                }
+                stop = input.IndexOf(";", pos);
+                DarkS = input.Substring(pos, stop - pos-2);
+
+                //line = RamanS + "\t" + DarkS + "\r\n";
+                line = RamanS + "\t" + DarkS;     
+
+                using (StreamWriter sw = File.AppendText(newPath))
+                {
+                    sw.WriteLine(line);
+                }
+                pos = input.IndexOf("\n",pos);
+                int b = 0;
+            }
+            
+
+
+            int a = 0;
+        }
+
+
+
+
+
+
+    }
+}
